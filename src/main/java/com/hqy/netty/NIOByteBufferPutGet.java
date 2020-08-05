42b5f206634e3897943af5489109a851cfa2dbc2
Huqunyang <328580301@qq.com>
2020-08-06 06:47:07 +0800

    sd

---
+++ src/main/java/com/hqy/netty/NIOByteBufferPutGet.java
@@ -0,0 +1,21 @@
+package com.hqy.netty;
+
+import java.nio.ByteBuffer;
+
+public class NIOByteBufferPutGet {
+    public static void main(String[] args) {
+
+        ByteBuffer byteBuffer = ByteBuffer.allocate(64);
+        for (int i = 0; i < 64; i++) {
+            byteBuffer.put((byte)i);
+        }
+
+        byteBuffer.flip();
+
+        ByteBuffer readOnlyBuffer = byteBuffer.asReadOnlyBuffer();
+        //readOnly类型为HeapByteBufferR
+        System.out.println(readOnlyBuffer.getClass());
+
+
+    }
+}
